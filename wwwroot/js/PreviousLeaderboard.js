ad155c3aeeb7996ef128a379178f00fcc6ece1f5
Rory Murnane <46994753+RM-118425146@users.noreply.github.com>
2022-08-09 16:51:09 +0100

    initial commit

---
+++ wwwroot/js/PreviousLeaderboard.js
@@ -0,0 +1,52 @@
+function authenticate() {
+    return gapi.auth2.getAuthInstance()
+        .signIn({scope: ""})
+        .then(function() { console.log("Sign-in successful"); },
+              function(err) { console.error("Error signing in", err); });
+  }
+  function loadClient() {
+    gapi.client.setApiKey("AIzaSyCMTmKWgb0Ox0PJsaYh0uC29jzDGUnin0k");
+    return gapi.client.load("https://sheets.googleapis.com/$discovery/rest?version=v4")
+        .then(function() { console.log("GAPI client loaded for API"); },
+              function(err) { console.error("Error loading GAPI client for API", err); });
+  }
+  // Make sure the client is loaded and sign-in is complete before calling this method.
+  function execute1() {
+    return gapi.client.sheets.spreadsheets.values.get({
+      "spreadsheetId": "1NiEV_4su7zp38TeUDGx63HORvWTZEOWzh6XQCW89r1Y",
+      "range": "'Elite'!B3"
+    })
+        .then(function(response) {
+                // Handle the results here (response.result has the parsed body).
+                console.log("Response", response);
+                document.getElementById('EliteWinner').textContent = response.result.values[0].toString();
+              },
+              function(err) { console.error("Execute error", err); });
+  }
+  function execute2(){
+    return gapi.client.sheets.spreadsheets.values.get({
+            "spreadsheetId": "1NiEV_4su7zp38TeUDGx63HORvWTZEOWzh6XQCW89r1Y",
+            "range": "'Premier'!B3"
+          })
+              .then(function(response) {
+                      // Handle the results here (response.result has the parsed body).
+                      console.log("Response", response);
+                      document.getElementById('PremWinner').textContent = response.result.values[0].toString();
+                    },
+                    function(err) { console.error("Execute error", err); });
+  }
+  function execute3(){
+    return gapi.client.sheets.spreadsheets.values.get({
+              "spreadsheetId": "1NiEV_4su7zp38TeUDGx63HORvWTZEOWzh6XQCW89r1Y",
+              "range": "'Championship'!B3"
+            })
+                .then(function(response) {
+                        // Handle the results here (response.result has the parsed body).
+                        console.log("Response", response);
+                        document.getElementById('champWinner').textContent = response.result.values[0].toString();
+                      },
+                      function(err) { console.error("Execute error", err); });
+  }
+  gapi.load("client:auth2", function() {
+    gapi.auth2.init({client_id: "1004677156648-kftf2851sr1h4rsv43uqkrffmcmcle07.apps.googleusercontent.com"});
+  });
